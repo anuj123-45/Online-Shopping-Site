87c09271a7b4f10faddd589ec46c00ab56638352
anuj123-45 <anuj1358.be20@chitkara.edu.in>
2022-09-17 15:17:10 +0530

    changes done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ result.products.map((value)=>{
           <p class="card-text">${value.description}</p>
           <p class="card-price"><b>Rs </b>${value.price}</p>
 
-         <button  class="btn btn-primary" onClick="addToCart(${id})"  >Add to cart</button>
+         <button  class="btn btn-primary add-to" onClick="addToCart(${id})"  >Add to cart</button>
         </div>
         
       </div>`;
@@ -45,10 +45,12 @@ var store=document.getElementsByClassName("card-img-top");
 console.log(store);
 
 var tableData="";
-// ids.add(id);
+
+var delR=0;
+
 function addToCart(id){
 
-console.log(result.products[id].title , result.products[id].price);
+
 var title=result.products[id].title;
 var price=result.products[id].price;
 var image=result.products[id].thumbnail;
@@ -59,17 +61,47 @@ var image=result.products[id].thumbnail;
     return;
   }
 
+  console.log(result.products[id].title , result.products[id].price);
+//tableData+=`<tr><td><img src="${image}" width="50%" height="50"></td><td>${title}</td><td><input type="number" value="1" style="width:34px"></td><td>${price}</td><td ><i class="fa-sharp fa-solid fa-trash" class="delCart" onClick="delRow(${id})"></i></td></tr>`;
 
-tableData+=`<tr><td><img src="${image}" width="50%" height="50"></td><td>${title}</td><td><input type="number" value="1" style="width:34px"></td><td>${price}</td><td><i class="fa-sharp fa-solid fa-trash"></i></td></tr>`;
+var tab=$('#foot');
+var resu=`<tr><td><img src="${image}" width="75%" height="50"></td><td>${title}</td><td><input type="number" value="1" style="width:64px"></td><td>${price}</td><td ><i class="fa-sharp fa-solid fa-trash" class="delCart" onClick="delRow(${id})"></i></td></tr>`
+tab.append(resu);
+
 ids.add(id);
 
+//document.getElementById("foot").innerHTML=tableData;
 
-
-document.getElementById("foot").innerHTML=tableData;
-
+delR++;
 
 }
 
 
+var table=document.getElementById("foot");
+function delRow(mainId){
+  ids.delete(mainId);
+  for(var i = 0; i < table.rows.length; i++)
+  {
+      table.rows[i].cells[4].onclick = function()
+      {
+          
+              index = this.parentElement.rowIndex;
+              table.deleteRow(index);
+           
+     
+      };
+      
+  }
 
 
+
+  console.log("Item deleted");
+}
+
+
+$(document).ready(function () {
+  $('#exampleModal').modal({
+         backdrop: 'static',
+         keyboard: false
+  })
+ });
